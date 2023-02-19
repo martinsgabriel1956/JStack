b240070c98e3d2601aae422836d8904cbf9072ae
Gabriel Martins <martinsgabriel@gmail.com>
2023-02-18 23:26:19 -0300

    feat: Creating the first API route

--- first-module/server/src/index.js
+++ first-module/server/src/index.js
@@ -1,8 +1,25 @@
 const http = require("http");
+const users = require("./mocks/users");
 
 const server = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/html" });
-  response.end("<h1>Hello World</h1>");
+  const routeLog = {
+    method: request.method,
+    endpoint: request.url,
+  };
+
+  const isGetUsersDataRoute =
+    routeLog.endpoint === "/users" && routeLog.method === "GET";
+
+  if (isGetUsersDataRoute) {
+    response.writeHead(200, { "Content-Type": "application/json" });
+    const usersData = JSON.stringify(users);
+    response.end(usersData);
+  } else {
+    response.writeHead(404, { "Content-Type": "text/html" });
+    response.end(`Cannot ${routeLog.method} ${routeLog.endpoint}`);
+  }
+
+  console.log(routeLog);
 });
 
 server.listen(3000, () => {
---
+++ first-module/server/src/mocks/users.js
@@ -0,0 +1,30 @@
+module.exports = [
+  {
+    id: 1,
+    name: "Gabriel",
+  },
+  {
+    id: 2,
+    name: "Maria",
+  },
+  {
+    id: 3,
+    name: "Regina",
+  },
+  {
+    id: 4,
+    name: "João",
+  },
+  {
+    id: 5,
+    name: "Leticia",
+  },
+  {
+    id: 6,
+    name: "Fernando",
+  },
+  {
+    id: 7,
+    name: "Patrick",
+  },
+];
