671c9833ef345a2229dcf42d266133728ff32def
Gabriel Martins <martinsgabriel@gmail.com>
2023-02-25 18:30:09 -0300

    feat: Query params

--- first-module/server/src/controllers/userController.js
+++ first-module/server/src/controllers/userController.js
@@ -2,8 +2,19 @@ const users = require("../mocks/users");
 
 module.exports = {
   listUser(request, response) {
+    const { order } = request.query;
+
+    const sortUsers = (prev, curr) => {
+      const orderIsDesc = order === "desc";
+      if (orderIsDesc) {
+        return prev.id < curr.id ? 1 : -1;
+      }
+      return prev.id > curr.id ? 1 : -1;
+    };
+
+    const sortedUsers = users.sort(sortUsers);
     response.writeHead(200, { "Content-Type": "application/json" });
-    const usersData = JSON.stringify(users);
+    const usersData = JSON.stringify(sortedUsers);
     response.end(usersData);
   },
 };
--- first-module/server/src/index.js
+++ first-module/server/src/index.js
@@ -1,14 +1,22 @@
 const http = require("http");
+const { URL } = require("url");
 const routes = require("./routes");
 const PORT = 3000;
 
 const server = http.createServer((request, response) => {
+  const host = request.headers.host;
+  const url = `http://${host}${request.url}`;
+  const parsedUrl = new URL(url);
+
   const findRoute = (routeObj) =>
-    routeObj.method === request.method && routeObj.endpoint === request.url;
+    routeObj.method === request.method &&
+    routeObj.endpoint === parsedUrl.pathname;
 
   const route = routes.find(findRoute);
 
   if (route) {
+    const searchParams = Object.fromEntries(parsedUrl.searchParams);
+    request.query = searchParams;
     route.handler(request, response);
   } else {
     response.writeHead(404, { "Content-Type": "application/json" });
